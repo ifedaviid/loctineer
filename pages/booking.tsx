5c4e04c9c81bc9af01146f43ece69327d4bad68c
IfeoluwaDavid <ifeoluwaadese@ymail.com>
2022-09-18 17:18:10 -0400

    removed unnecessary custom hook

--- pages/booking.tsx
+++ pages/booking.tsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import Link from "next/link";
 import Button from "../components/button";
 import Layout from "../components/layout";
-import Link from "next/link";
+import {
+    SelectInterest,
+    SelectService,
+    ConfirmBooking
+} from '../components/booking/index';
+import { updateInterest, updateService } from './../actions/booking';
 import styles from "./booking.module.scss";
-import useBooking from "../components/booking/useBooking";
 
 const Booking = () => {
-    const {
-        bookingStages,
-        currentStage,
-        currentStageIndex,
-        goToStage
-    } = useBooking();
+    const dispatch = useDispatch();
+    const [currentStageIndex, setCurrentStageIndex] = useState(0);
+    const [interest, setInterest] = useState(null);
+    const [service, setService] = useState(null);
+
+    const bookingStages = [
+        {
+            title: "What are you interested in?",
+            component: <SelectInterest interest={interest} setInterest={setInterest} />,
+        },
+        {
+            title: `Choose a ${interest} service`,
+            component: <SelectService interest={interest} service={service} setService={setService} />,
+        },
+        {
+            title: `Thank you! See you soon!`,
+            component: <ConfirmBooking />,
+        }
+    ];
+
+    const saveSelection = () => {
+        switch (currentStageIndex) {
+            case 0:
+                dispatch(updateInterest(interest));
+                break;
+            case 1:
+                dispatch(updateService(service));
+                break;
+            default:
+                break;
+        }
+    }
+
+    const goToStage = (index, clickedNext) => {
+        const isValidIndex = index >= 0 || index < bookingStages.length - 1
+        if (isValidIndex) setCurrentStageIndex(index);
+        if (clickedNext) saveSelection();
+    };
+
     return (
         <Layout>
-            <h2 className={styles.bookingHeader}>{currentStage.title}</h2>
-            {currentStage.component}
+            <h2 className={styles.bookingHeader}>{bookingStages[currentStageIndex].title}</h2>
+            {bookingStages[currentStageIndex].component}
             <div className={styles.buttonGroupContainer}>
                 <div className={styles.stageNavigationButtonGroup}>
                     {currentStageIndex !== 0 &&
