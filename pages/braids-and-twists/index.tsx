9529c276abe1d6c66f2f7a5b4dd01ddd8ee4673a
IfeoluwaDavid <ifeoluwaadese@ymail.com>
2022-11-06 02:49:20 -0500

    updated service-type pages

--- pages/braids-and-twists/index.tsx
+++ pages/braids-and-twists/index.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import Layout from "../../components/layout";
-import Carousel from "../../components/carousel";
 import Listing from "../../components/listing";
-import { featuredPhotos } from "../../data/featured-photos";
-import { serviceTypes } from "../../data";
+import { braidsAndTwistsServices } from "../../data";
 import * as strings from '../../data/strings';
 
 export default function BraidsAndTwists() {
-    const braidsAndTwistsInfo = serviceTypes.find(service => service.name === strings.BRAIDS_AND_TWISTS)
-    const { services } = braidsAndTwistsInfo;
     return (
         <Layout>
             <section className='gray'>
-                <h2>{strings.BRAIDS_AND_TWISTS} Services</h2>
+                <h2>Braids and Twists Services</h2>
                 <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}</p>
                 <div className="services-content">
-                    {services.map((option, idx) => {
+                    {braidsAndTwistsServices.map((option, idx) => {
                         const { image, name, description, cta } = option;
                         return (
                             <Listing
@@ -29,12 +25,6 @@ export default function BraidsAndTwists() {
                         );
                     })}
                 </div>
-            </section>
-            <section className="dark">
-                <h2>Gallery</h2>
-                <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}</p>
-                <Carousel images={featuredPhotos} />
             </section>
         </Layout >
     );
--- pages/dreadlocks/index.tsx
+++ pages/dreadlocks/index.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import Layout from "../../components/layout";
 import Listing from "../../components/listing";
-import { serviceTypes } from "../../data";
-import * as strings from '../../data/strings';
+import { dreadlocksServices } from "../../data";
 
 export default function Dreadlocks() {
-    const dreadlocksInfo = serviceTypes.find(service => service.name === strings.LOCS)
-    const { services } = dreadlocksInfo;
     return (
         <Layout>
             <section className='gray'>
-                <h2>{strings.LOCS} Services</h2>
+                <h2>Dreadlocks Services</h2>
                 <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}</p>
                 <div className="services-content">
-                    {services.map((option, idx) => {
+                    {dreadlocksServices.map((option, idx) => {
                         const { image, name, description, cta } = option;
                         return (
                             <Listing
