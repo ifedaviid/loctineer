96d8e4a5ba8d9c8470ef90df4077442be48d1834
iadese-godaddy <ifeoluwaadese@ymail.com>
2022-12-03 14:09:49 -0500

    omitted files

--- components/booking/confirm-booking.tsx
+++ components/booking/confirm-booking.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import styles from "./select.module.scss";
 
 const ConfirmBooking = () => {
     return (
-        <h2 className={styles.bookingHeader}>Checkout: Review your appointment details</h2>
+        <h2>Checkout: Review your appointment details</h2>
     );
 };
 
