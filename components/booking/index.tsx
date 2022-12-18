fba0bb08e66084c38fe60f8e57a068b813b92637
iadese-godaddy <ifeoluwaadese@ymail.com>
2022-12-18 02:15:45 -0500

    added confirmation.tsx + removed booking-completed.tsx

--- components/booking/index.tsx
+++ components/booking/index.tsx
@@ -3,4 +3,4 @@ export { default as ExtensionUsageModal } from "./adding-extensions";
 export { default as SelectExtensionLength } from "./select-extension-length";
 export { default as SelectHairLength } from "./select-hair-length";
 export { default as SelectSchedule } from "./select-schedule";
-export { default as BookingCompleted } from "./booking-completed";
+export { default as Confirmation } from "./confirmation";
