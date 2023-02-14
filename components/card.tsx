009b2a962b351c242beaea3c4c50782bc340177b
iadese-godaddy <ifeoluwaadese@ymail.com>
2023-02-14 02:35:31 -0500

    card menu UX improvements (1/2)

--- components/card.tsx
+++ components/card.tsx
@@ -1,41 +1,66 @@
-import React from 'react'
-import CustomImage from './custom-image';
-import styles from './card.module.scss';
-import { AppImage } from '../types/image';
+import React, { useState } from "react";
+import CustomImage from "./custom-image";
+import styles from "./card.module.scss";
+import { AppImage } from "../types/image";
 
 interface Props {
-    title?: string;
-    description?: string;
-    onChange?: Function;
-    category?: string;
-    isSelected?: boolean;
-    image?: AppImage;
+  title?: string;
+  description?: string;
+  onChange?: any;
+  category?: string;
+  isSelected?: boolean;
+  image?: AppImage;
 }
 
 const Card = ({
-    title,
-    description,
-    onChange,
-    category,
-    isSelected,
-    image
+  title,
+  description,
+  onChange,
+  category,
+  isSelected,
+  image,
 }: Props) => {
+  // const [darken, setDarken] = useState(true);
+  // const handleClick = () => ;
+  return (
+    // <div
+    //   role="radio"
+    //   // checked={isSelected}
+    //   aria-checked={isSelected}
+    //   // onKeyDown={onChange ? () => onChange() : null}
+    //   // tabIndex={0}
+    //   // label={payLaterText}
+    //   // aria-label={payLaterText}
+    //   // name={RADIO_GROUP_NAME}
 
-    return (
-        <div
-            className={`${styles.card} ${isSelected ? styles.active : styles.inactive}`}
-            // tabIndex={0}
-            onClick={onChange ? () => onChange() : null}
-        // onKeyDown={onChange ? () => onChange() : null}
-        >
-            {image && <CustomImage image={image} />}
-            <div className={styles.cardDetails}>
-                {category && <small>{category}</small>}
-                <h4>{title}</h4>
-                {description && <p>{description}</p>}
-            </div>
-        </div>
-    );
-}
+    //   // aria-checked={selectedPayLater}
+    //   // onChange={() => handleSelection(PAYLATER)}
+    // >
+    //   {image && <CustomImage image={image} />}
+    //   <div className={styles.cardDetails}>
+    //   </div>
+    // </div>
+
+    <label
+      className={`${styles.card} ${
+        isSelected ? styles.active : styles.inactive
+      }`}
+      onClick={onChange ? () => onChange() : null}
+    >
+      <input
+        name="plan"
+        type="radio"
+        className={styles.radio}
+        checked={isSelected}
+      />
+      {image && <CustomImage image={image} darken={!isSelected}/>}
+      <div className={styles["plan-details"]}>
+        {category && <span className={styles["plan-type"]}>{category}</span>}
+        <h4>{title}</h4>
+        {description && <p>{description}</p>}
+      </div>
+    </label>
+  );
+};
 
 export default Card;
--- components/custom-image.tsx
+++ components/custom-image.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import Image from 'next/image';
-import { AppImage } from '../types/image';
-import styles from './custom-image.module.scss';
+import React from "react";
+import Image from "next/image";
+import { AppImage } from "../types/image";
+import classNames from "classnames";
+import styles from "./custom-image.module.scss";
 
 interface Props {
-    image: AppImage
+  image: AppImage;
+  roundEdged?: boolean;
+  darken?: boolean;
 }
 
-const CustomImage = ({ image }: Props) => {
-    const { path, altText } = image;
-    return (
-        <div className={styles['custom-image']}>
-            <Image
-                src={path}
-                alt={altText}
-                placeholder='blur'
-            />
-        </div>
-    )
-}
+const CustomImage = ({ image, roundEdged, darken }: Props) => {
+  const { path, altText } = image;
+  return (
+    <div
+      className={classNames(styles["custom-image"], {
+        [styles.rounded]: roundEdged,
+        [styles.darken]: darken,
+      })}
+    >
+      <Image src={path} alt={altText} placeholder="blur" />
+    </div>
+  );
+};
 
 export default CustomImage;
