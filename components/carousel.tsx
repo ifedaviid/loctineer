f5fe7686eb56f0cea6b8e8ffeca2f765ab4cdbaf
iadese-godaddy <104440018+iadese-godaddy@users.noreply.github.com>
2023-02-26 00:14:15 -0500

    testing cssMode to fix choppy slide issue

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -15,6 +15,7 @@ interface Props {
 const Carousel = ({ images }: Props) => {
   return (
     <Swiper
+      cssMode={true}
       breakpoints={{
         320: { slidesPerView: 1.2 },
         520: { slidesPerView: 1.2 },
