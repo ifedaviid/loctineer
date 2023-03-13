7a9787717387ab4824ec057f3e9d11817d7cd86e
galaxydeve <devphs513@gmail.com>
2023-03-13 11:55:12 +0800

    modify exit confirm dlg style

--- components/booking/confirm-exit.tsx
+++ components/booking/confirm-exit.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "../button";
 import {
+  Box,
   Dialog, DialogActions,
   DialogContent, DialogContentText, DialogTitle,
 } from '@mui/material';
 
 import { useState } from 'react';
+import styles from "./confirm-exit.module.scss";
+import Modal from "../modal";
 
 const useConfirm = (title: string, message: string): [() => JSX.Element, () => Promise<unknown>] => {
   const [promise, setPromise] = useState(null);
@@ -27,21 +30,74 @@ const useConfirm = (title: string, message: string): [() => JSX.Element, () => P
     promise?.resolve(false);
     handleClose();
   };
-  // You could replace the Dialog with your library's version
+
   const ConfirmationDialog = () => (
-    <Dialog
-      open={promise !== null}
-      fullWidth
-    >
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>{message}</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button variant="primary" onClick={handleConfirm}>Yes</Button>
-        <Button variant="secondary" onClick={handleCancel}>No</Button>
-      </DialogActions>
-    </Dialog>
+    // <Dialog
+    //   open={promise !== null}
+    //   fullWidth
+    //   sx={{ borderRadius: '2rem' }}
+    // >
+    //   <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
+    //     <Box>
+    //       {title}
+    //     </Box>
+    //     <Box>
+    //       <a
+    //         href="#"
+    //         onClick={handleCancel}
+    //       >
+    //         x
+    //       </a>
+    //     </Box>
+    //   </DialogTitle>
+    //   <DialogContent>
+    //     <DialogContentText>{message}</DialogContentText>
+    //   </DialogContent>
+    //   <DialogActions className={styles.buttonGroup}>
+    //     <Button variant="secondary" onClick={handleCancel}>No</Button>
+    //     <Button variant="primary" onClick={handleConfirm}>Yes</Button>
+    //   </DialogActions>
+    // </Dialog>
+    <>
+      {promise !== null &&
+        <Modal>
+          {" "}
+          {/* This is the dark overlay in the background */}
+          <div className={styles.modalContainer}>
+            {" "}
+            {/* This is the modal container */}
+            <div>
+              <h4>{title}</h4>
+              <div>
+                <a
+                  href="#"
+                  onClick={handleCancel}
+                >
+                  x
+                </a>
+              </div>
+            </div>
+            <p>
+              {message}
+            </p>
+            <div className={styles.buttonGroup}>
+              <Button
+                variant="secondary"
+                onClick={handleCancel}
+              >
+                No, thanks
+              </Button>
+              <Button
+                variant="primary"
+                onClick={handleConfirm}
+              >
+                Yes, I will!
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      }
+    </>
   );
   return [ConfirmationDialog, confirm];
 };
