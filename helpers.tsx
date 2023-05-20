c3e203cb1c19b945bbe7676f496204752d2f7096
iadese-godaddy <iadese@godaddy.com>
2023-05-20 18:55:42 -0400

    helpers.tsx as standalone root file

---
+++ helpers.tsx
@@ -0,0 +1,7 @@
+import { AppImage } from "./types/image";
+
+export function extractImages(data) {
+  let images: Array<AppImage> = [];
+  data.map((obj) => images.push(obj.image));
+  return images;
+}
