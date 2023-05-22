ff51a1c38d525cdb951adeda78f783a64ebf6768
iadese-godaddy <iadese@godaddy.com>
2023-05-21 23:12:59 -0400

    omitted these

--- pages/consult.tsx
+++ pages/consult.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../src/layout";
 
 const Booking = () => {
-    return (
-        <Layout>
-            <h2>Book a Consultation Appointment</h2>
-        </Layout >
-    );
+  return (
+    <Layout>
+      <h2>Book a Consultation Appointment</h2>
+    </Layout>
+  );
 };
 
 export default Booking;
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import Hero from "../src/hero";
 import Layout from "../src/layout";
 import Listing from "../src/listing";
 import CustomImage from "../src/custom-image";
-import { extractImages } from "../src/helpers/utils
+import { extractImages } from "../helpers";
 import { featuredImages } from "../data/featured-images";
 import { serviceTypes } from "../data";
 
