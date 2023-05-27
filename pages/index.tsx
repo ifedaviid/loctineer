8f4bf06d306e03b02b38aa9deee9586785150acc
iadese-godaddy <ifeoluwaadese@ymail.com>
2023-05-27 19:04:57 -0400

    added extension usage tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,18 +53,9 @@ export default function Home() {
         <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}</p>
         <div className="services-content">
-          {serviceTypes.map((option, idx) => {
-            const { image, name, description, cta } = option;
-            return (
-              <Listing
-                key={idx}
-                image={image}
-                title={name}
-                description={description}
-                cta={cta}
-              />
-            );
-          })}
+          {serviceTypes.map((option, idx) => (
+            <Listing key={idx} service={option} />
+          ))}
         </div>
       </section>
       <section className="dark">
--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Chip from "@mui/material/Chip";
 import Button from "src/components/button";
 import styles from "src/components/listing.module.scss";
-import { AppImage } from "src/types/image";
+import { Service } from "src/types/service";
+import { ExtensionUsage } from "src/types/extension-usage";
 
 interface Props {
-  image: AppImage;
-  title: string;
-  description: string;
-  cta: CallToAction;
+  service: Service;
 }
-const Listing = ({ image, title, description, cta }: Props) => {
+
+const Listing = ({ service }: Props) => {
+  const { name, image, description, cta, extensionUsage } = service;
+
+  let label = null;
+  switch (extensionUsage) {
+    case ExtensionUsage.NOT_OFFERED:
+      label = "Without hair extensions";
+      break;
+
+    case ExtensionUsage.POSSIBLE:
+      label = "Can use hair extensions";
+      break;
+
+    case ExtensionUsage.REQUIRED:
+      label = "Must use hair extensions";
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <div className={styles["listing"]}>
       <div>
@@ -23,7 +43,15 @@ const Listing = ({ image, title, description, cta }: Props) => {
         />
       </div>
       <div>
-        <h3>{title}</h3>
+        {label && (
+          <Chip
+            label={label}
+            variant="outlined"
+            size="small"
+            style={{ fontFamily: "inherit", color: "white" }}
+          />
+        )}
+        <h3>{name}</h3>
         <p>{description}</p>
         {cta.primary && (
           <Link href={cta.primary.href}>
