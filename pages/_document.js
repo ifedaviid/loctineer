9dcef77873b40653a6b3da0725d6046808ba4d21
iadese-godaddy <ifeoluwaadese@ymail.com>
2023-05-28 06:31:07 -0400

    added fb messenger chat config snippet

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,33 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `      
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "1665091083765739");
+            chatbox.setAttribute("attribution", "biz_inbox");`,
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `window.fbAsyncInit = function() {
+              FB.init({
+                xfbml: true,
+                version: 'v17.0'
+              });
+            };
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
+          }}
+        />
         <body>
           <Main />
           <NextScript />
