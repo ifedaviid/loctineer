b4fea024a0e95b410a56233ca0a4d0faeda91601
iadese-godaddy <ifeoluwaadese@ymail.com>
2023-06-02 03:27:34 -0400

    added back button to all services page

--- pages/services/[service-type]/index.tsx
+++ pages/services/[service-type]/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useRouter } from "next/router";
+import MuiButton from "@mui/material/Button";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import Layout from "src/template/page-wrapper";
 import Listing from "src/components/listing";
 import { braidsAndTwistsServices, dreadlocksServices, strings } from "data";
@@ -7,6 +10,8 @@ import { Service } from "src/types";
 let name: string, data: Array<Service>;
 
 export default function ServiceType({ serviceType }) {
+  const router = useRouter();
+
   switch (serviceType) {
     case strings.BRAIDS_AND_TWISTS_ID:
       name = strings.BRAIDS_AND_TWISTS;
@@ -22,6 +27,21 @@ export default function ServiceType({ serviceType }) {
   return (
     <Layout>
       <section className="gray">
+        <MuiButton
+          sx={{
+            textTransform: "none",
+            letterSpacing: "2px",
+            color: "#4a4f4f",
+            justifyContent: "flex-start",
+            padding: "0.5rem 0",
+            fontFamily: "inherit",
+          }}
+          size="medium"
+          startIcon={<ChevronLeftIcon />}
+          onClick={() => router.push("/services")}
+        >
+          All Services
+        </MuiButton>
         <h2>{name} Services</h2>
         <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}</p>
