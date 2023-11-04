fdf28b0ed38ad1f51934fd432e1b65575e55cce6
ifedaviid <ifeoluwaadese@ymail.com>
2023-11-03 20:11:49 -0400

    omitted: removed obsolete code for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,8 @@
 import React, { useState } from "react";
-import { useRouter } from "next/router";
-
-import { AppImage } from "src/types";
-import Modal from "src/components/modal";
 import Hero from "src/components/hero";
-import CustomImage from "src/components/custom-image";
 import Layout from "src/template/page-wrapper";
-import About from "src/sections/about";
-import ServiceList from "src/sections/service-list";
-import { serviceTypes } from "data";
-import Photos from "src/sections/photos";
 
 export default function HomePage() {
-  const router = useRouter();
-  const initialPopUpState = {
-    showing: false,
-    image: null,
-  };
-  const [popUp, setPopUp] = useState(initialPopUpState);
-
-  const openImage = (image: AppImage) => (
-    <Modal closeModal={() => setPopUp(initialPopUpState)}>
-      <CustomImage image={image} roundEdged />
-    </Modal>
-  );
-
   return (
     <Layout>
       <Hero />
