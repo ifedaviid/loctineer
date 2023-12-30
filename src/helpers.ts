71f77041ee3ada304b0c18a67284c8e6f270442a
ifedaviid <ifeoluwaadese@ymail.com>
2023-12-30 15:17:45 -0500

    added getServiceById helper function

---
+++ src/helpers.ts
@@ -0,0 +1,14 @@
+import { business } from "data/index";
+
+export function getServiceById(id: string) {
+    for (const service of business.services) {
+        if (service.id === id) return { service: service, parent: null };
+        if (service.services) {
+            const subServices = service.services;
+            for (const subservice of subServices) {
+                if (subservice.id === id) return { service: subservice, parent: service };
+            }
+        }
+    }
+    return undefined;
+}
