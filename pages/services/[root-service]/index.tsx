53ecfe9c6e432df5bf13d8acedd618879d0d6b8a
ifedaviid <ifeoluwaadese@ymail.com>
2023-12-31 23:21:17 -0500

    removed service menu component

--- pages/services/[root-service]/index.tsx
+++ pages/services/[root-service]/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useRouter } from "next/router";
+import MuiButton from "@mui/material/Button";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import Layout from "src/template/page-wrapper";
-import ServiceMenu from "src/components/service-menu";
+import Listing from "src/components/listing";
 import { getServiceById } from "src/helpers";
 import { Service } from "src/types";
 import { LOCS_ID, BRAIDS_AND_TWISTS_ID } from "data/strings";
@@ -8,13 +11,38 @@ import { LOCS_ID, BRAIDS_AND_TWISTS_ID } from "data/strings";
 export default function RootServicesPage({
   service: stringifiedServiceObj,
 }) {
+  const router = useRouter();
+  const returnRoute = { path: '/services', name: 'All Services' }
   const service: Service = JSON.parse(stringifiedServiceObj);
+  const { name, description, services } = service;
   return (
     <Layout>
-      <ServiceMenu
-        service={service}
-        returnRoute={{ path: '/services', name: 'All Services' }}
-      />
+      <section style={{ padding: '3rem 5%' }}>
+        {returnRoute && (
+          <MuiButton
+            sx={{
+              textTransform: "none",
+              letterSpacing: "2px",
+              color: "lightgray",
+              justifyContent: "flex-start",
+              padding: "0.5rem",
+              fontFamily: "inherit",
+            }}
+            size="medium"
+            startIcon={<ChevronLeftIcon />}
+            onClick={() => router.push(returnRoute.path)}
+          >
+            {returnRoute.name}
+          </MuiButton>
+        )}
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <div className="services-content" style={{ paddingTop: '1rem' }}>
+          {services.map((option, idx) => (
+            <Listing key={idx} service={option} />
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 }
