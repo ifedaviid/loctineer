cf07e99e4f1bd6619c9a26125973afdf410015b7
ifedaviid <ifeoluwaadese@ymail.com>
2024-02-13 01:54:03 -0500

    refactored listing component

--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/legacy/image";
 import Link from "next/link";
-import Button from "src/components/button";
 import styles from "src/components/listing.module.scss";
 import { Service } from "src/types";
 
@@ -10,28 +9,20 @@ interface Props {
 }
 
 const Listing = ({ service }: Props) => {
-  const { name, featuredImage, description, cta } = service
+  const { name, featuredImage, cta } = service
 
   return (
-    <div className={styles["listing"]}>
-      <div>
-        <Image
-          src={featuredImage.path}
-          alt={featuredImage.altText}
-          placeholder="blur"
-          loading="lazy"
-        />
-      </div>
-      <div>
-        <h3>{name}</h3>
-        <p>{description}</p>
-        {cta.primary && (
-          <Link href={cta.primary.href} legacyBehavior>
-            <Button variant="primary">{cta.primary.text}</Button>
-          </Link>
-        )}
-      </div>
-    </div>
+    <div className={styles.listing}>
+      <Image
+        src={featuredImage.path}
+        alt={featuredImage.altText}
+        placeholder="blur"
+        loading="lazy"
+        objectFit="cover"
+      />
+      <Link href={cta?.primary?.href} className={styles.overlay} />
+      <Link href={cta?.primary?.href} className={styles.title}>{name}</Link>
+    </div >
   );
 };
 
